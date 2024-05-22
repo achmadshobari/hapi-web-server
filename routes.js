fd29f340ac55fc38c1a717321497892e95474426
achmadshobari <achmadshobari@gmail.com>
2024-05-22 22:51:29 +0700

    menambahkan quey parameter

---
+++ routes.js
@@ -0,0 +1,52 @@
+const routes =[
+    {
+        method : 'GET',
+        path :'/',
+        handler :(request, h) =>{
+            return 'Homepage';
+        },
+    },
+    {
+        method : '*',
+        path :'/',
+        handler :(request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tesebut';
+        },
+    },
+    {
+        method : 'GET',
+        path :'/about',
+        handler :(request, h) =>{
+            return 'About page';
+        },
+    },
+    {
+        method : '*',
+        path :'/about',
+        handler :(request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tesebut';
+        },
+    },
+    {
+        method : 'GET',
+        path :'/hello/{name?}',
+        handler : (request, h) => {
+            const {name = "starenger"} = request.params;
+            const {lang} = request.query;
+            if(lang==='id'){
+                return `Hai ${name}`;
+            }
+            return `Hello, ${name}`;
+        },
+
+    },
+    {
+        method : '*',
+        path :'/{any*}',
+        handler :(request, h) => {
+            return 'Halaman tidak ditemukant';
+        },
+    }
+]
+
+module.exports = routes;
